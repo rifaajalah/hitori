3f5f7a34f98c6e4cc5e8823810804c650532ec57
rifaajalah <ullfaaajaaa21@gmail.com>
2025-06-20 01:28:36 +0700

    Ubah setting

--- settings.js
+++ settings.js
@@ -10,9 +10,9 @@ const chalk = require('chalk');
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
 global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
-global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.packname = '𝙨𝙚𝙣𝙛𝙖𝙖𝘽𝙊𝙏☆ • Bot WhatsApp'
+global.author = 'Rifajalah'
+global.botname = '𝙨𝙚𝙣𝙛𝙖𝙖𝘽𝙊𝙏☆'
 global.listprefix = ['+','!','.']
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
