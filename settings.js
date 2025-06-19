b0956853863e1d9a79c68eba0ba7b6c3ccd4cf7c
rifaajalah <ullfaaajaaa21@gmail.com>
2025-06-20 01:25:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,36 +9,36 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
-global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.owner = ['6289528950840'] //['628','628'] 2 owner atau lebih
+global.packname = '𝙨𝙚𝙣𝙛𝙖𝙖𝘽𝙊𝙏☆ • Bot WhatsApp'
+global.author = 'rifaajalah'
+global.botname = '𝙨𝙚𝙣𝙛𝙖𝙖𝘽𝙊𝙏☆'
 global.listprefix = ['+','!','.']
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
 global.tempatStore = 'baileys_store.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
 global.pairing_code = true
-global.number_bot = '' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
+global.number_bot = '6288708050512' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
 
 global.fake = {
 	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
-	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
-	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	thumbnailUrl: 'https://files.catbox.moe/b21b25.jpg',
+	thumbnail: fs.readFileSync('./download/lastfile/thumbnail.jpg'),
 	docs: fs.readFileSync('./src/media/fake.pdf'),
 	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
 }
 
 global.my = {
-	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
+	ig: 'https://www.instagram.com/ch0comilkiess_?igsh=bmY2MHk4MDAyanRt',
+	ch: 'https://whatsapp.com/channel/0029VbAxF8q8fewqyB0tF90P',
 	gc: 'https://chat.whatsapp.com/B5qJIwZHm4VEYZJQE6iMwy',
 	ch: '120363250409960161@newsletter',
 }
 
 global.limit = {
-	free: 20,
-	premium: 999,
-	vip: 9999
+	free: 999,
+	premium: 9999,
+	vip: 99999
 }
 
 global.money = {
